bd21a36a1cdc2d604f3843745baac58ce12672fe
Vinícius Delmo <vinicius.delmo@gmail.com>
2022-11-09 17:20:43 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# ➡️CALCULADORA - C/ TYPESCRIPT⬅️
+
+[Objetivo](#Objetivo) |
+[Aprendizado](#Aprendizado) |
+[Melhorias](#Melhorias-futuras)
+
+## 🎯Objetivos:
+O presente estudo tem por obejtivo a criação de aplicação WEB de calculadora utilizando o **typescript**. A página deve:
+
+- [x] Dois imputs para inserção de número
+- [x] Realizar soma
+- [x] Realizar subtração
+- [x] Utilziar typescript
+
+
+## 👨‍🏫Aprendizado:
+> No estudo foi possível aplicar conhecimentos de **typescript** e boas práticas de programação.
+
+## 🚀Melhorias futuras:
+
+1. Inserir HTML/CSS/Bootstrap
+2. Adicionar divisão e subtração
+3. Adicionar porcentagem
+4. Adicionar histórico de operações
+
+
+## 👨‍💻 Tecnologias utilizadas:
+* HTML5
+* CSS3
+* Javascript
+* Typescript
+
+## 🔗Link de acesso: -
+#### 🔄Última Atualização: 25/10/2022
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+const input1 = document.getElementById('num1') as HTMLInputElement;
+const input2 = document.getElementById('num2') as HTMLInputElement;
+const buttonSoma = document.getElementById('buttonSomar') as HTMLElement;
+const buttonSubtrair = document.getElementById('buttonSubtrair') as HTMLElement;
+
+
+
+/*function somar({ a, b}: Valores): number{
+  return a + b;
+}
+function subtrair({ a, b}: Valores): number{
+  return a - b;
+}x
+
+buttonSoma.addEventListener('click', function(){
+  const resultado = somar({
+    a: Number(input1.value),
+    b: Number(input2.value)
+  })
+  console.log(resultado);
+})
+
+buttonSubtrair.addEventListener('click', function(){
+  const resultado = subtrair({
+    a: Number(input1.value),
+    b: Number(input2.value)
+  })
+  console.log(resultado);
+})*/
+
+type Operacoes = "SOMAR" | "SUBTRAIR"
+
+interface Valores{
+  tipo: Operacoes;
+  a: number;
+  b: number;
+}
+function operacao({ tipo, a, b}: Valores): number{
+  if(tipo === "SOMAR"){
+    return a + b;
+  }else{
+    return a - b;
+  }
+}
+buttonSoma.addEventListener('click', function(){
+  const resultado = operacao({
+    tipo: "SOMAR",
+    a: Number(input1.value),
+    b: Number(input2.value)
+  })
+  console.log(resultado);
+})
+
+buttonSubtrair.addEventListener('click', function(){
+  const resultado = operacao({
+    tipo: "SUBTRAIR",
+    a: Number(input1.value),
+    b: Number(input2.value)
+  })
+  console.log(resultado);
+})
